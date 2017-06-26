ea6fb36a676b11b93838703c7c873e0285f41196
Alfonso Fidalgo <Alfonso.Fidalgo@transferwise.com>
2017-06-26 17:11:53 +0100

    first commit

---
+++ js/dashboard.js
@@ -0,0 +1,155 @@
+// Load the Visualization API and the controls package.
+google.charts.load('current', {'packages':['corechart', 'controls']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawSheetName);
+
+//query spreadsheet, executed OnLoad
+function drawSheetName() {
+  var queryString = encodeURIComponent("SELECT A, B, sum(J), sum(I)/sum(J), sum(I), sum(K)/sum(G), sum(I)/sum(H), sum(H)/sum(G), sum(J)/sum(H), sum(H), D, L, C GROUP BY A, B, D, L, C");
+
+  var query = new google.visualization.Query(
+      'https://docs.google.com/spreadsheets/d/1Mw9VkPaciMng7HMmVTa-s7nmIpqdpzyNWsorX7xyJnE/gviz/tq?sheet=Sheet1&headers=1&tq=' + queryString);
+  query.send(handleSampleDataQueryResponse);
+};
+
+//handle response from spreadsheet
+function handleSampleDataQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  };
+  var data = response.getDataTable();
+
+  var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard_div'));
+
+  // Create all the filters
+  var rangeSlider = new google.visualization.ControlWrapper({
+    'controlType': 'DateRangeFilter', //'DateRangeFilter'
+    'containerId': 'filter_div',
+    'options': {
+      'filterColumnLabel': 'Week'
+    }
+  });
+
+  var catFilter = new google.visualization.ControlWrapper({
+    'controlType': 'CategoryFilter',
+    'containerId': 'filter_divB',
+    'options': {
+      'filterColumnLabel': 'Category',
+      'ui': {'allowMultiple': false}
+    },
+    'state': {'selectedValues': ['Generic']}
+  });
+
+  var devFilter = new google.visualization.ControlWrapper({
+    'controlType': 'CategoryFilter',
+    'containerId': 'filter_divC',
+    'options': {
+      'filterColumnLabel': 'Device',
+      'ui': {'allowMultiple': false}
+    },
+    'state': {'selectedValues': ['Computers']}
+  });
+
+  var mtFilter = new google.visualization.ControlWrapper({
+    'controlType': 'CategoryFilter',
+    'containerId': 'filter_divD',
+    'options': {
+      'filterColumnLabel': 'Match Type',
+      'ui': {'allowMultiple': false}
+    },
+    'state': {'selectedValues': ['Broad']}
+  });
+
+  var slotFilter = new google.visualization.ControlWrapper({
+    'controlType': 'CategoryFilter',
+    'containerId': 'filter_divE',
+    'options': {
+      'filterColumnLabel': 'Top vs. Other',
+      'ui': {'allowMultiple': false}
+    },
+    'state': {'selectedValues': ['Top']}
+  });
+
+//Create all the charts
+  var lineChart = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'chart1',
+    'options': {
+      //'height': 400,
+      'legend': 'bottom',
+      'title': 'Conversions & CPA',
+      'series': {
+        0: {targetAxisIndex: 0, labelInLegend: "Conversions"},
+        1: {targetAxisIndex: 1, labelInLegend: "CPA"}
+      },
+      'vAxes': {
+        0: {'title': 'Conversions'},
+        1: {'title': 'CPA', 'format': 'currency'}
+      }
+    },
+    'view': {'columns': [0, 2, 3]}
+  });
+
+  var lineChart2 = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'chart2',
+    'options': {
+      //'height': 400,
+      'legend': 'bottom',
+      'title': 'Cost & Avg. Position',
+      'series': {
+        0: {targetAxisIndex: 0, labelInLegend: "Cost"},
+        1: {targetAxisIndex: 1, labelInLegend: "Avg. Position"}
+      },
+      'vAxes': {
+        0: {'title': 'Cost', 'format': 'currency'},
+        1: {'title': 'Avg. Position'}
+      }
+    },
+    'view': {'columns': [0, 4, 5]}
+  });
+
+  var lineChart3 = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'chart3',
+    'options': {
+      //'height': 400,
+      'legend': 'bottom',
+      'title': 'CPC & CTR',
+      'series': {
+        0: {targetAxisIndex: 0, labelInLegend: "CPC"},
+        1: {targetAxisIndex: 1, labelInLegend: "CTR"}
+      },
+      'vAxes': {
+        0: {'title': 'CPC', 'format': 'currency'},
+        1: {'title': 'CTR', 'format': 'percent'}
+      }
+    },
+    'view': {'columns': [0, 6, 7]}
+  });
+
+  var lineChart4 = new google.visualization.ChartWrapper({
+    'chartType': 'LineChart',
+    'containerId': 'chart4',
+    'options': {
+      //'height': 400,
+      'legend': 'bottom',
+      'title': 'CVR & Clicks',
+      'series': {
+        0: {targetAxisIndex: 0, labelInLegend: "CVR"},
+        1: {targetAxisIndex: 1, labelInLegend: "Clicks"}
+      },
+      'vAxes': {
+        0: {'title': 'CVR', 'format': 'percent'},
+        1: {'title': 'Clicks', 'format': 'decimal'}
+      }
+    },
+    'view': {'columns': [0, 8, 9]}
+  });
+
+//Bind and draw
+  dashboard.bind([rangeSlider, devFilter, catFilter, mtFilter, slotFilter], [lineChart, lineChart2, lineChart3, lineChart4]);
+  dashboard.draw(data);
+};
---
+++ js/sendAndDraw.js
@@ -0,0 +1,14 @@
+google.charts.load('current', {'packages' : ['table']});
+google.charts.setOnLoadCallback(function() { sendAndDraw('') });
+
+var dataSourceUrl = 'https://docs.google.com/spreadsheets/d/1Mw9VkPaciMng7HMmVTa-s7nmIpqdpzyNWsorX7xyJnE/gviz/tq?sheet=Sheet1&headers=1&tq=';
+var query;
+
+function sendAndDraw(queryString) {
+  var container = document.getElementById('orgchart');
+  var chart = new google.visualization.Table(container);
+  query && query.abort();
+  query = new google.visualization.Query(dataSourceUrl + queryString);
+  var queryWrapper = new QueryWrapper(query, chart, {'page': 'enable', 'pageSize': 15}, container);
+  queryWrapper.sendAndDraw();
+}
